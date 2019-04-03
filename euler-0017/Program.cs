1fbde102a7e0ec835308fb9ea374f04007540fb5
Nick <ntorgov@gmail.com>
2019-04-03 10:01:27 +0300

    Unfinished

--- euler-0017/Program.cs
+++ euler-0017/Program.cs
@@ -34,5 +34,48 @@ namespace euler_0017
 
 			Console.WriteLine("Hello World!");
 		}
+
+		public void Parser(int Number)
+		{
+			string Value = Number.ToString();
+			int Length = Value.Length;
+			var digits = 0;
+			var tens = 0;
+			var hundrets = 0;
+			var thousands = 0;
+
+			if (Length >= 1)
+			{
+				digits = int.Parse(Value.Substring(Length - 1, 1));
+			}
+			if (Length >= 2)
+			{
+				tens = int.Parse(Value.Substring(Length - 2, 1));
+			}
+			if (Length >= 3)
+			{
+				hundrets = int.Parse(Value.Substring(Length - 3, 1));
+			}
+			if (Length >= 4)
+			{
+				thousands = int.Parse(Value.Substring(Length - 4, 1));
+			}
+
+			if (tens == 1 && (digits == 1 || digits == 2))
+			{
+				///
+			}
+			else
+			{
+				var digitalWord = "";
+				foreach(var word in list)
+				{
+					if (word.Number == digits)
+					{
+						digitalWord = word.Word;
+					}
+				}
+			}
+		}
 	}
 }
