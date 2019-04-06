4eaf03ab4e822d71bcb6f858ff1b2a2f89597dba
Nick <ntorgov@gmail.com>
2019-04-06 15:07:32 +0300

    F# test

---
+++ euler-0001/Program.fs
@@ -0,0 +1,23 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+
+    for n in 1 .. 1000 do
+        let i = isValid(n);
+
+    0 // return an integer exit code
+
+// What's a fuck???
+let isValid value =
+    let Result = false;
+
+    let isThree value = (value % 3) = 0;
+    let isFive value = (value % 5) = 0;
+
+    if isFive then Result=true else Result=false
+
+    Result
