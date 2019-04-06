3e2233623387ddc029e4d6e89a94a261fba895ce
Nick <ntorgov@gmail.com>
2019-04-06 15:29:36 +0300

    Solved

--- euler-0001/Program.fs
+++ euler-0001/Program.fs
@@ -2,22 +2,27 @@
 
 open System
 
+// What's a fuck???
+let isValid value =
+    //let Result = false;
+
+    let is_Three =(value % 3) = 0;
+    let is_Five =(value % 5) = 0;
+
+    let mutable Result = (is_Three || is_Five)
+
+    Result
+
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
 
-    for n in 1 .. 1000 do
-        let i = isValid(n);
+    let mutable FinalResult = 0
+
+    for n = 1 to 999 do
+        let i = isValid n
+        if i then FinalResult <- FinalResult + n
+
+    printfn "Result is %d" FinalResult
 
     0 // return an integer exit code
-
-// What's a fuck???
-let isValid value =
-    let Result = false;
-
-    let isThree value = (value % 3) = 0;
-    let isFive value = (value % 5) = 0;
-
-    if isFive then Result=true else Result=false
-
-    Result
