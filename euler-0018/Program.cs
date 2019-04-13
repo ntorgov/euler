27f16c4152b674462d18787259d0b11cb8fb1c69
Nick <ntorgov@gmail.com>
2019-04-13 10:00:17 +0300

    Not solved

--- euler-0018/Program.cs
+++ euler-0018/Program.cs
@@ -38,11 +38,31 @@ namespace euler_0018
 		private static int[,] DataArray;
 		static void Main(string[] args)
 		{
+			/*
 			DataArray = new int[,] {
 				{3, 0, 0, 0},
 				{7, 4, 0, 0},
 				{2, 4, 6, 0},
 				{8, 5, 9, 4}
+			};
+			*/
+
+			DataArray = new int[,] {
+				{75, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00},
+				{95, 64, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00},
+				{17, 47, 82, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00},
+				{18, 35, 87, 10, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00},
+				{20, 04, 82, 47, 65, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00},
+				{19, 01, 23, 75, 03, 34, 00, 00, 00, 00, 00, 00, 00, 00, 00},
+				{88, 02, 77, 73, 07, 63, 67, 00, 00, 00, 00, 00, 00, 00, 00},
+				{99, 65, 04, 28, 06, 16, 70, 92, 00, 00, 00, 00, 00, 00, 00},
+				{41, 41, 26, 56, 83, 40, 80, 70, 33, 00, 00, 00, 00, 00, 00},
+				{41, 48, 72, 33, 47, 32, 37, 16, 94, 29, 00, 00, 00, 00, 00},
+				{53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14, 00, 00, 00, 00},
+				{70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57, 00, 00, 00},
+				{91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48, 00, 00},
+				{63, 66, 04, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31, 00},
+				{04, 62, 98, 27, 23, 09, 70, 98, 73, 93, 38, 53, 60, 04, 23}
 			};
 
 			long Result = 0;
@@ -53,8 +73,6 @@ namespace euler_0018
 
 			List<RouteModel> Routes = new List<RouteModel>();
 
-			//for (var routeId = 0; routeId < RouteLength; routeId++)
-			//{
 			int Counter = 0;
 			for (var y = yLength - 1; y >= 0; y--)
 			{
@@ -83,8 +101,7 @@ namespace euler_0018
 					{
 						continue;
 					}
-					//for (var n = 0; n <= Routes.Count; n++)
-					//{
+
 					// берем маршруты снизу слева
 					List<RouteModel> leftRoutes = Routes.Where(s => s.PositionX.Equals(nearestLeft)).Where(s => s.PositionY.Equals(y + 1)).ToList();
 					foreach (var element in leftRoutes)
@@ -123,9 +140,6 @@ namespace euler_0018
 							Routes.First(r => r.Id.Equals(element.Id)).Value += DataArray[y, x];
 						}
 					}
-
-					Console.WriteLine("Line");
-					//}
 
 					maxRowValue = Math.Max(maxRowValue, DataArray[y, x]);
 				}
