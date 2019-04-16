e67171286c6aa4da0e13fe4cd223884675393e60
Nick <ntorgov@gmail.com>
2019-04-16 08:09:47 +0300

    Начало

---
+++ euler-0019/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace euler_0019
+{
+	/// <summary>
+	/// <list type="bullet">
+	/// <listheader><term>Дана следующая информация</term><description>(однако, вы можете проверить ее самостоятельно):</description></listheader>
+	/// <item><description>1 января 1900 года - понедельник.</description></item>
+	/// <item><description>В апреле, июне, сентябре и ноябре 30 дней.</description></item>
+	/// <item><description>В феврале 28 дней, в високосный год - 29.</description></item>
+	/// <item><description>В остальных месяцах по 31 дню.</description></item>
+	/// <item><description>Високосный год - любой год, делящийся нацело на 4, однако последний год века (ХХ00) является високосным в том и только том случае, если делится на 400.</description></item>
+	/// </list>
+	/// <para>Сколько воскресений выпадает на первое число месяца в двадцатом веке (с 1 января 1901 года до 31 декабря 2000 года)?</para>
+	/// </summary>
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
