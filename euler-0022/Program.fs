db23563c6100b499dd08dddd1a356c79c879f553
Nick <ntorgov@gmail.com>
2019-04-26 21:53:51 +0300

    Progress

--- euler-0022/Program.fs
+++ euler-0022/Program.fs
@@ -1,8 +1,22 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.IO
+
+let readLines (filePath:string) = seq {
+    Console.WriteLine(filePath)
+    use sr = new StreamReader (filePath)
+    while not sr.EndOfStream do
+        yield sr.ReadLine ()
+}
 
 [<EntryPoint>]
 let main argv =
+    let filePath = "../../../p022_names.txt";
+
+    let result = readLines(filePath)
+
     printfn "Hello World from F#!"
+    Console.ReadLine();
     0 // return an integer exit code
+
