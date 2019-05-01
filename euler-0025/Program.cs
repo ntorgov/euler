9b09631db720dfe1da7b5fe2c9bb081ab666216d
Nick <ntorgov@gmail.com>
2019-05-01 21:00:15 +0300

    Solved

---
+++ euler-0025/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Numerics;
+
+namespace euler_0025
+{
+	class Program
+	{
+		private static BigInteger currentValue = 1;
+		private static BigInteger previousValue = 0;
+		private static BigInteger Index = 1;
+
+		static void Main(string[] args)
+		{
+			var currentLength = currentValue.ToString().Length;
+			while (currentLength < 1000)
+			{
+				var indx = Fib();
+				currentLength = currentValue.ToString().Length;
+			}
+			Console.WriteLine("Result is " + Index);
+			Console.WriteLine("Hello World!");
+		}
+
+		private static BigInteger Fib()
+		{
+			var tempValue = currentValue;
+			currentValue = currentValue + previousValue;
+			previousValue = tempValue;
+			Index++;
+
+			return Index;
+		}
+	}
+}
